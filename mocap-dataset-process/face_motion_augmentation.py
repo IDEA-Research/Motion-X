62b0cd744959eb39d05d750b2865d31e40a674fa
linjing7 <70081899+linjing7@users.noreply.github.com>
2024-01-23 17:25:55 +0800

    Update face_motion_augmentation.py

--- mocap-dataset-process/face_motion_augmentation.py
+++ mocap-dataset-process/face_motion_augmentation.py
@@ -1,13 +1,8 @@
-import argparse
-import json
 import os
 import numpy as np
 from tqdm import tqdm
-
 import torch
-from slerp import slerp_interpolate
-from multiprocessing import Pool
-
+import torch.nn.functional as F
 
 def findAllFile(base):
     file_path = []
@@ -17,63 +12,25 @@ def findAllFile(base):
             file_path.append(fullname)
     return file_path
 
+motion_folder = '../datasets/motion_data/smplx_322'
 
-motion_folder = "../datasets/motion_data/smplx_322"
+for mocap_dataset in ['humanml', 'EgoBody', 'GRAB']:
 
-
-def face_motion_augmentation(mocap_motion_file):
-    face_motion_file = mocap_motion_file.replace("/motion_data/", "/face_motion_data/")
-    motion = np.load(mocap_motion_file)
-    if os.path.exists(face_motion_file):
+    mocap_motion_folder = os.path.join(motion_folder, mocap_dataset)
+    mocap_motion_files = findAllFile(mocap_motion_folder)
+    for mocap_motion_file in tqdm(mocap_motion_files):
+        face_motion_file = mocap_motion_file.replace('/motion_data/', '/face_motion_data/')
+        motion = np.load(mocap_motion_file)
+        motion_length = motion.shape[0]
         face_motion = np.load(face_motion_file)
-    else:
-        return face_motion_file  # not found face motion file
 
-    motion_length, face_motion_length = motion.shape[0], face_motion.shape[0]
-    if motion_length != face_motion_length:
-        face_motion = torch.from_numpy(face_motion)
-        n_frames, n_dims = face_motion.shape
-        n_joints = n_dims // 3
-        face_motion = face_motion.reshape(n_frames, n_joints, 3)
-        face_motion = slerp_interpolate(face_motion, motion_length)
-        face_motion = face_motion.reshape(motion_length, -1).numpy()
-    (
-        motion[:, 66 + 90 : 66 + 93],
-        motion[:, 159 : 159 + 50],
-        motion[:, 209 : 209 + 100],
-    ) = (face_motion[:, :3], face_motion[:, 3 : 3 + 50], face_motion[:, 53:153])
-    np.save(mocap_motion_file, motion)
-    return None
+        # perform motion interpolation to avoid frame rate mismatch
+        if face_motion.shape[0] != motion_length:
+            face_motion = torch.from_numpy(face_motion)
+            face_motion = face_motion[None].permute(0, 2, 1)  # [1, num_feats, num_frames]
+            face_motion = F.interpolate(face_motion, size=motion_length, mode='linear')  # motion interpolate
+            face_motion = face_motion.permute(0, 2, 1)[0].numpy()  # [num_frames, num_feats]
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--num_proc", type=int, default=len(os.sched_getaffinity(0)) // 2
-    )
-    args = parser.parse_args()
-
-    not_found_face_motion_files = []
-    for mocap_dataset in ["humanml", "EgoBody", "GRAB"]:
-        mocap_motion_folder = os.path.join(motion_folder, mocap_dataset)
-
-        mocap_motion_files = findAllFile(mocap_motion_folder)
-        with Pool(args.num_proc) as p:
-            with tqdm(total=len(mocap_motion_files)) as pbar:
-                for not_found_face_motion_file in p.imap_unordered(
-                    face_motion_augmentation, mocap_motion_files
-                ):
-                    not_found_face_motion_files.append(not_found_face_motion_file)
-                    pbar.update(1)
-
-    not_found_face_motion_files = list(
-        filter(lambda x: x is not None, not_found_face_motion_files)
-    )
-
-    print(
-        "The count of not_found_face_motion_files: ", len(not_found_face_motion_files)
-    )
-    json.dump(
-        not_found_face_motion_files,
-        open(os.path.join("./", "not_found_face_motion_files.json"), "w"),
-    )
+        motion[:, 66 + 90:66 + 93], motion[:, 159:159 + 50], motion[:, 209:209 + 100] = \
+            face_motion[:, :3], face_motion[:, 3:3+50], face_motion[:, 53:153]
+        np.save(mocap_motion_file, motion)
